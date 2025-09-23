c4f5fe7a27c887680a89e2d4e8a77bab88445d99
patwilson3 <patrick.wilson2@mail.mcgill.ca>
2025-09-23 15:58:28 -0400

    added constant.py, gpio_mode_map is incomplete

---
+++ GPIO Library/constants.py
@@ -0,0 +1,6 @@
+gpio_mode_map = {1<<1 : "Kernel: in use by kernal", 1<<2 : "Kernel: Output", 
+                 1<<3 : "Kernel: Active low", 1<<4: "Open drain", 
+                 1<<5 : "Kernel: Open source", 1<<6: "Kernel: Pull up set",
+                 1<<7 : "Kernel: Pull down set"}
+
+#TO BE COMPLETED
