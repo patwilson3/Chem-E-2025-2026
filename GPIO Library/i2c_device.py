000c0b7f3ec97d48fe56951d55bba9cd5eb90f35
patwilson3 <patrick.wilson2@mail.mcgill.ca>
2025-09-24 11:17:44 -0400

    i2c

---
+++ GPIO Library/i2c_device.py
@@ -0,0 +1,9 @@
+import lgpio
+
+class I2C_Device:
+    def __init__(self, addr, i2c_bus):
+        self._addr = addr
+        self._i2c_bus = i2c_bus
+        self._device_handle = lgpio.i2c_open(addr, i2c_bus)
+    
+        
