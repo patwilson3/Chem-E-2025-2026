ab8da183518cba732b8824262ef943efac282e45
patwilson3 <146208676+patwilson3@users.noreply.github.com>
2025-09-29 19:20:08 -0400

    Update i2c_device.py

--- GPIO Library/i2c_device.py
+++ GPIO Library/i2c_device.py
@@ -4,7 +4,7 @@ class I2C_Device:
     def __init__(self, addr, i2c_bus):
         self._addr = addr
         self._i2c_bus = i2c_bus
-        self._device_handle = lgpio.i2c_open(addr, i2c_bus)
+        self._device_handle = lgpio.i2c_open(i2c_bus, addr)
     
     def close(self):
         lgpio.i2c_close(self._handle)
