f7cd47ac6f3cc0526d5546ab5836004beebd075e
patwilson3 <patrick.wilson2@mail.mcgill.ca>
2025-10-22 10:09:16 -0400

    added init function, incomplete)

--- GPIO_Library/a4988.py
+++ GPIO_Library/a4988.py
@@ -1,4 +1,27 @@
 import lgpio
 
 class AD4988:
-    pass
+    def __init__(self, pins:dict):
+        if not isinstance(pins, dict):
+            raise TypeError("Pins must be a dictionary")
+        self.MS1 = pins['MS1']
+        self.MS2 = pins['MS2']
+        self.MS3 = pins['MS3']
+        self.RESET = pins['RESET']
+        self.SLEEP = pins['SLEEP']
+        self.STEP = pins['STEP']
+        self.GND = pins['GND']
+        self.DIR = pins['DIR']
+        self.ENABLE = pins['ENABLE']
+
+        
+    
+
+
+
+
+
+
+
+
+ 
